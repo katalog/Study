56d5e97686d6d4e2cf9e56b3ea530e357ae6f005
eunkyoon kim <katalog@gmail.com>
2024-07-23 15:23:11 +0200

    Update script.js

--- Practice_JS/novel/script.js
+++ Practice_JS/novel/script.js
@@ -17,7 +17,7 @@ async function fetchNovelContent(url) {
     }
 
     let modContent = cleanText(content.innerHTML);
-    modContent = modContent.replace(/^\n+/gm, '')
+    modContent = modContent.replace(/^\n\n\n+/gm, '')
     modContent = "#." + modContent
     return modContent
 }
